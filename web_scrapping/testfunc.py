ccfa1ec8111f7d69816afbd18df779647ee80093
RbMo7 <rbmohit1283@gmail.com>
2023-12-30 17:15:58 +0545

    changed into block

--- web_scrapping/testfunc.py
+++ web_scrapping/testfunc.py
@@ -1,8 +1,9 @@
 from text import *
 import threading
 
-thread_one = threading.Thread(target=get_reviews_amazon())
-thread_two = threading.Thread(target=after_func("the north face jacket"))
+def scrap(keyword):
+    thread_one = threading.Thread(target=get_reviews_amazon())
+    thread_two = threading.Thread(target=after_func(keyword))
 
-thread_one.start()
-thread_two.start()
+    thread_one.start()
+    thread_two.start()
